12b71f27c03baa9518ae2bc8cc883674031184e6
urassh <shuto.1011@icloud.com>
2025-12-28 22:06:56 +0900

    first commit

---
+++ Swappy/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swappy
+//
+//  Created by 浦山秀斗 on 2025/12/28.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swappy/SwappyApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwappyApp.swift
+//  Swappy
+//
+//  Created by 浦山秀斗 on 2025/12/28.
+//
+
+import SwiftUI
+
+@main
+struct SwappyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
