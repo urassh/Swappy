73b22e9dc1899cce4e6d6e1c3588175764ed8708
Palm <241it002i@s.internet.ac.jp>
2026-01-02 22:27:57 +0900

    Top完成

--- android/app/src/main/java/com/swappy/sample/MainActivity.kt
+++ android/app/src/main/java/com/swappy/sample/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.swappy.sample
 
 import android.os.Bundle
+import android.graphics.RenderEffect
+import android.graphics.Shader
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import com.swappy.sample.databinding.ActivityMainBinding
 
@@ -12,5 +15,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+            val blur = RenderEffect.createBlurEffect(50f, 50f, Shader.TileMode.CLAMP)
+            binding.joinGlow.setRenderEffect(blur)
+        }
+
+        binding.startJoinContainer.setOnClickListener {
+            // TODO: join flow
+        }
     }
 }
