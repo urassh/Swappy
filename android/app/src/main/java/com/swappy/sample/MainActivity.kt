0cd3d1a1eb6e70823a2d81461bc5820bcd291c21
Palm <241it002i@s.internet.ac.jp>
2026-01-03 22:19:07 +0900

    待機場一旦

--- android/app/src/main/java/com/swappy/sample/MainActivity.kt
+++ android/app/src/main/java/com/swappy/sample/MainActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.graphics.RenderEffect
 import android.graphics.Shader
 import android.os.Build
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import com.swappy.sample.databinding.ActivityMainBinding
 
@@ -19,10 +20,35 @@ class MainActivity : AppCompatActivity() {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
             val blur = RenderEffect.createBlurEffect(50f, 50f, Shader.TileMode.CLAMP)
             binding.joinGlow.setRenderEffect(blur)
+            binding.waitingJoinGlow.setRenderEffect(blur)
         }
 
         binding.startJoinContainer.setOnClickListener {
-            // TODO: join flow
+            binding.startContent.visibility = View.GONE
+            binding.waitingContent.visibility = View.VISIBLE
+        }
+
+        var isCameraOn = true
+        var isMicOn = false
+
+        binding.waitingCameraToggle.setOnClickListener {
+            isCameraOn = !isCameraOn
+            val cameraIcon = if (isCameraOn) {
+                R.drawable.ic_waiting_camera_on
+            } else {
+                R.drawable.ic_waiting_camera_off
+            }
+            binding.waitingCameraToggle.setImageResource(cameraIcon)
+        }
+
+        binding.waitingMicToggle.setOnClickListener {
+            isMicOn = !isMicOn
+            val micIcon = if (isMicOn) {
+                R.drawable.ic_waiting_mic_on
+            } else {
+                R.drawable.ic_waiting_mic_off
+            }
+            binding.waitingMicToggle.setImageResource(micIcon)
         }
     }
 }
